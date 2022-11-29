b4e97873c9e21f6161d2e85f3991198416d06565
Andrea Verdura <andrea.verdura@damiani.com>
2022-11-29 10:30:47 +0100

    Added basic login page

--- warehouseproject/urls.py
+++ warehouseproject/urls.py
@@ -19,4 +19,5 @@ from django.urls import include, path
 urlpatterns = [
     path('', include('warehouse_app.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
