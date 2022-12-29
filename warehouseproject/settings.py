600a50c26dff100242f5a1e2269d92caa3445847
AVDamInt <andrea.verdura@damiani.com>
2022-12-29 16:18:09 +0100

    Modified alloewd hosts

--- warehouseproject/settings.py
+++ warehouseproject/settings.py
@@ -27,10 +27,10 @@ SECRET_KEY = 'django-insecure-fyzgjcbm-zpdx*o)9csvts$w4*d$f=5pj9c67pz3e@e7g2=x(+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://daminventory-staging.azurewebsites.net"]
+ALLOWED_HOSTS = ["https://daminventory-staging.azurewebsites.net","daminventory-staging.azurewebsites.net"]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://daminventory-staging.azurewebsites.net'
+    'https://daminventory-staging.azurewebsites.net','daminventory-staging.azurewebsites.net'
 ]
 
 # Application definition
