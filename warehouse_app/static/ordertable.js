e70d95cea2f28bf4495eed1f9f679003971fefcb
verduand <andrea.verdura@damiani.com>
2023-03-28 18:18:57 +0200

    Added link to brother printer in menu and button to print in detail page

--- warehouse_app/static/ordertable.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-  $('#sortedtable').DataTable();
-  document.getElementById("sortedtable_length").style.visibility = "hidden";
-  document.getElementById("sortedtable_paginate").style.visibility = "hidden";
-  document.getElementById("sortedtable_info").style.visibility = "hidden";
-  document.getElementById("sortedtable_filter").style.visibility = "hidden";
-});
