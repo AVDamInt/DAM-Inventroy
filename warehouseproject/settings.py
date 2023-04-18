800ebc32eed9a91de80c62bb1a7b4f028bf91582
verduand <andrea.verdura@damiani.com>
2023-04-18 09:34:40 +0200

    Removed wrong cls in settings.py

--- warehouseproject/settings.py
+++ warehouseproject/settings.py
@@ -19,7 +19,7 @@ from decouple import config, AutoConfig
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-cls
+
 config = AutoConfig(search_path='.env')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
