98cbd8d1f065b67da4b264a373c39758ee6d6d88
Andrea Verdura <andrea.verdura@damiani.com>
2023-08-29 16:05:59 +0200

    Added allowed host

--- warehouseproject/settings.py
+++ warehouseproject/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["damianinventory-staging.azurewebsites.net"]
+ALLOWED_HOSTS = ["damianinventory-staging.azurewebsites.net","169.254.130.2"]
 
 #ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
